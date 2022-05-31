ad06ff86916f85eea02cf6d6030f9bfc4e82a7b7
Jaime Dias <jaimejosediasnt@gmail.com>
2022-05-31 20:05:33 -0300

    Adicionando migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(StateTableSeeder::class);
+        $this->call(CityTableSeeder::class);
+        $this->call(AxisTableSeeder::class);
     }
 }
